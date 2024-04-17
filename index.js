563174ec06412e99f93059e8dcf95e581396c8ed
vivek-l-yadav <vivekly199315@gmail.com>
2024-04-17 22:36:01 +0530

    learning git

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("career restart--")
+let arr = [1,2,3,4]
+let res = arr.map((a) =>  a==4 )
+console.log(res)
+// 2. filter
+let arr = [1,2,3,4]
+let res = arr.filter((a) =>  a==4 )
+console.log(res)
+
+// 3. reduce
+let arr = [1,2,3,4]
+let res = arr.reduce((a,b) =>  a+b )
